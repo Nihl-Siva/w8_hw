14b1443589453cf2edff1f13136c304fdae4f0ba
Dylan Cutsforth <dylancutsforth@gmail.com>
2023-02-13 15:19:54 -0600

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home, Dashboard, SignIn } from './components';
+import './styles.css'
+import reportWebVitals from './reportWebVitals';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home title  ={'Drones Inventory'}/>} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/signin' element={<SignIn />} />
+        </Routes>
+      </Router>
+
+    
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
